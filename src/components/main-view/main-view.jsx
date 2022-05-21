e71fcb1d76d6c69138fe16d5e11b5aeda7748cda
Marina Nadj <marinanadj7@yahoo.com>
2022-05-21 21:14:51 +0200

    3,3

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,40 +1,14 @@
 import React from 'react';
 
-
-import { MovieCard } from '../movie-card/movie-card';
-import { MovieView } from '../movie-view/movie-view';
-
-
-
-
 export class MainView extends React.Component {
-constructor(){ 
-    super();
-    this.state = {
-    movies: [],
-    selectedMovies:null
-  };
-}
 
-
-setSelectedMovie(newSelectedMovie) { 
-     this.setState({selectedMovie: newSelectedMovie });
-     }
-
-render() {
-    const { movies, selectedMovie } = this.state;
-    if (movies.length === 0) return <div className="main-view">The list is empty!</div>;
+  render() {
     return (
-        <div className="main-view">
-            {selectedMovie
-                    ? <MovieView movie={selectedMovie} onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }} />
-                    : movies.map((movie) => (
-                        <MovieCard key={movie._id} movie={movie} onMovieClick={(newSelectedMovie) => { this.setSelectedMovie(newSelectedMovie) }} />
-                    ))
-                }
-            </div>
-        );
-    }
-
+      <div className="main-view">
+        <div>Inception</div>
+        <div>The Shawshank Redemption</div>
+        <div>Gladiator</div>
+      </div>
+    );
+  }
 }
-    
