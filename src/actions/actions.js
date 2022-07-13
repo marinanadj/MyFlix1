e1baee867bcb3e20ae93bb08e6b0f13f7b4303dd
Marina Nadj <marinanadj7@yahoo.com>
2022-07-13 19:21:43 +0200

    člmČm,

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -1,45 +1,42 @@
 export const SET_MOVIES = 'SET_MOVIES';
 export const SET_FILTER = 'SET_FILTER';
-export const SET_FAVORITE = 'SET_FAVORITE';
-export const TOGGLE_FAVORITE = 'TOGGLE_FAVORITE';
 export const SET_USER = 'SET_USER';
-export const SET_TREND_SORT = 'SET_TREND_SORT';
-export const TOGGLE_TREND_SORT = 'TOGGLE_TREND_SORT';
-export const SET_MOV_SORT = 'SET_MOV_SORT';
-export const TOGGLE_MOV_SORT = 'TOGGLE_MOV_SORT';
+export const ADD_FAVMOVIE = 'ADD_FAVMOVIE';
+export const REM_FAVMOVIE = 'REM_FAVMOVIE';
+
 
 export function setMovies(value) {
-  return { type: SET_MOVIES, value };
+    return { 
+        type: SET_MOVIES, 
+        value 
+    };
 }
 
 export function setFilter(value) {
-  return { type: SET_FILTER, value };
-}
-
-export function setFavorites(value) {
-  return { type: SET_FAVORITE, value };
-}
-
-export function toggleFavorite(movieId) {
-  return { type: TOGGLE_FAVORITE, movieId };
+    return { 
+        type: SET_FILTER, 
+        value 
+    };
 }
 
 export function setUser(user) {
-  return { type: SET_USER, user };
+    console.log("SETUSER", user)
+    return {
+        type: SET_USER,
+        user: user
+    };
 }
 
-export function setTrendingSort(sortArr) {
-  return { type: SET_TREND_SORT, sortArr };
+export function addFavMovie(value) {
+    return {
+        type: ADD_FAVMOVIE,
+        value
+    }
 }
 
-export function toggleTrendingSort(toggle) {
-  return { type: TOGGLE_TREND_SORT, toggle };
-}
-
-export function setMovieSort(sortArr) {
-  return { type: SET_MOV_SORT, sortArr };
-}
-
-export function toggleMovieSort(toggle) {
-  return { type: TOGGLE_MOV_SORT, toggle };
+export function remFavMovie(value) {
+    return {
+        type: REM_FAVMOVIE,
+        value
+    };
 }
